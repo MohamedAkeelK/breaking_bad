bee2006f7e760e694e507bca5bc396463f86146e
mohamed khan <mohamedkhan@mohameds-MacBook-Pro.local>
2022-09-29 15:12:24 -0400

    updating info when button is clicked

--- script2.js
+++ script2.js
@@ -6,6 +6,12 @@
 let prev = document.querySelector(".prev");
 let content = document.querySelector(".content");
 let charImg = document.querySelector(".char-img");
+let charName = document.querySelector(".name");
+let birthday = document.querySelector(".birthday");
+let occupation = document.querySelector(".occupation");
+let charStatus = document.querySelector(".status");
+let nickname = document.querySelector(".nickname");
+let portrayed = document.querySelector(".portrayed");
 
 async function storeQuotes() {
   let response = await axios("https://www.breakingbadapi.com/api/quotes");
@@ -26,14 +32,29 @@ async function getCharacters() {
   let response = await axios("https://www.breakingbadapi.com/api/characters");
 
   let characters = [...response.data];
+  console.log(characters);
   let i = 0;
   content.addEventListener("click", (e) => {
     if (e.target.classList.contains("prev") && i > 0) {
+      //
       i--;
       charImg.src = characters[i].img;
+      charName.innerText = "Name: " + characters[i].name;
+      birthday.innerText = "Birthday: " + characters[i].birthday;
+      occupation.innerText = characters[i].occupation[0];
+      charStatus.innerText = "Status: " + characters[i].status;
+      nickname.innerText = "Nickname: " + characters[i].nickname;
+      portrayed.innerText = "Portrayed: " + characters[i].portrayed;
+      //
     } else if (e.target.classList.contains("next")) {
       i++;
       charImg.src = characters[i].img;
+      charName.innerText = "Name: " + characters[i].name;
+      birthday.innerText = "Birthday: " + characters[i].birthday;
+      occupation.innerText = characters[i].occupation[0];
+      charStatus.innerText = "Status: " + characters[i].status;
+      nickname.innerText = "Nickname: " + characters[i].nickname;
+      portrayed.innerText = "Portrayed: " + characters[i].portrayed;
     }
   });
 }
